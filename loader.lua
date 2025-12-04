86a21d65215f4647283c756eb7636fdbb0ed6dfc
Wic1k <dragonmarxa3@gmail.com>
2025-12-04 11:57:46 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,3 +3,5 @@ if game.PlaceId == 71184132329621 then
 elseif game.PlaceId == 99323319663859 then
   loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/f1c9a9ac86c5a593c839d02f3355fc7c32ef6693afd322d309c5102a1ec22800/download"))() -- survive on a raft
 end
+
+setclipboard("https://discord.gg/eJ3y94X6Eu")
