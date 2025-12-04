7977fa025dc11ded49cf510e234a378bd065f1f0
Wic1k <dragonmarxa3@gmail.com>
2025-12-04 11:42:55 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 71184132329621 then
-  loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/06245a015e3d068a0e4be59993ab14c7531dd8e21f9d5087b5ca8cc9218181ad/download"))()
+  loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/06245a015e3d068a0e4be59993ab14c7531dd8e21f9d5087b5ca8cc9218181ad/download"))() -- purgatory
+elseif game.PlaceId == 99323319663859 then
+  loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/f1c9a9ac86c5a593c839d02f3355fc7c32ef6693afd322d309c5102a1ec22800/download"))() -- survive on a raft
 end
