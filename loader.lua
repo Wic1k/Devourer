a250509e53fd3f65f2383d87ff54f7aa7861db08
Wic1k <dragonmarxa3@gmail.com>
2025-12-04 08:35:16 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,3 @@
-prin("yes")
+if game.PlaceId == 71184132329621 then
+  loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/06245a015e3d068a0e4be59993ab14c7531dd8e21f9d5087b5ca8cc9218181ad/download"))()
+end
